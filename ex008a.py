bf57a7a3fe29e43cc1585bc8ba794984213c6b49
Eric Adam <eadamfs.programador@gmail.com>
2022-09-18 19:05:20 -0300

    finalizando ex008a

--- ex008a.py
+++ ex008a.py
@@ -1,6 +1,16 @@
-metros = float(input('Digite o valor em metros: '))
+m = float(input('Digite o valor em metros: '))
 
-centimetros = metros*100
-milimetros = metros*1000
+km = m*0.001
+hm = m*0.01
+dam = m*0.1
+dm = m*10
+cm = m*100
+mm = m*1000
 
-print(f'{centimetros}cm')
+print(f'\nA medida de {m:.2f}m corresponde a:')
+print(f'{km:.3f}km')
+print(f'{hm:.3f}hm')
+print(f'{dam:.3f}dam')
+print(f'{dm:.2f}dm')
+print(f'{cm:.2f}cm')
+print(f'{mm:.2f}mm')
