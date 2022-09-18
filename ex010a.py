ef36964be2a384a7b8aacdad9b9fd7aa51009b87
Eric Adam <eadamfs.programador@gmail.com>
2022-09-18 19:41:02 -0300

    ex010a

---
+++ ex010a.py
@@ -0,0 +1,7 @@
+valorDigitado = float(input('Quanto você dejesa converter: R$ '))
+
+valorDolar = 5.25
+
+valorConvertido = valorDigitado/valorDolar
+
+print(f'Com R$ {valorDigitado:.2f} você pode comprar US$ {valorConvertido:.2f}')
