0bc7a0d6d39cc915e0c1f730c4c777243d46e0e5
Eric Adam <eadamfs.programador@gmail.com>
2022-09-18 20:11:26 -0300

    ex013a

---
+++ ex013a.py
@@ -0,0 +1,5 @@
+salarioFuncionario = float(input('Digite o valor correspondente ao salário do funcionário: R$ '))
+
+novoSalario = salarioFuncionario+salarioFuncionario*15/100
+
+print(f'Um funcionário que recebe R$ {salarioFuncionario:.2f}, com 15% de aumento, passará a receber {novoSalario:.2f}')
